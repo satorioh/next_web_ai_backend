c6e381a54bb3856183c363cde3c57dddaeeb67aa
Robin.Wang <3Jg$94sbQ@q1>
2024-07-02 17:29:26 +0800

    chore(gunicorn): change timeout

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,7 +4,7 @@ import multiprocessing
 bind = '0.0.0.0:2096'  # 绑定ip和端口号
 backlog = 512  # 可服务的客户端数量
 # chdir = '/home/test/server/bin' #gunicorn要切换到的目的工作目录
-timeout = 30  # 超时
+timeout = 300  # 超时
 worker_class = 'uvicorn.workers.UvicornWorker'  # 使用gevent模式，还可以使用sync 模式，默认的是sync模式
 # workers = multiprocessing.cpu_count() * 2 + 1  # 进程数
 workers = 1  # 进程数
